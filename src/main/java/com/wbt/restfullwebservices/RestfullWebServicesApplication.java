54c5c7d138d1564e46ef4f905db56589f4b2be50
rsingh2489 <rsingh2489@gmail.com>
2020-01-03 17:48:32 +0530

    first commit 7

--- src/main/java/com/wbt/restfullwebservices/RestfullWebServicesApplication.java
+++ src/main/java/com/wbt/restfullwebservices/RestfullWebServicesApplication.java
@@ -2,14 +2,15 @@ package com.wbt.restfullwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestfullWebServicesApplication {
+public class RestfullWebServicesApplication extends SpringBootServletInitializer {
 
- /*@Override
  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
   return application.sources(RestfullWebServicesApplication.class);
- }*/
+ }
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfullWebServicesApplication.class, args);
